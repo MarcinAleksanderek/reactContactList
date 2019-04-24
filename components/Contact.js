6e75433edf39c4ff46179ea4b07f8ab94b2568fb
Marcin <marcin.aleksanderek@gmail.com>
2019-04-24 23:43:17 +0200

    Add application styles

---
+++ components/Contact.js
@@ -0,0 +1,23 @@
+var Contact = React.createClass({
+	propTypes: {
+	item: React.PropTypes.object.isRequired,
+	},
+
+	render: function() {
+	return (
+		React.createElement('div', {className: 'contactItem'},
+		React.createElement('img', {
+			className: 'contactImage',
+			src: './images/' + this.props.item.firstName + this.props.item.lastName + '.png'
+		}),
+		React.createElement('div', {className: 'contactData'},
+			React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+			React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+			React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+				this.props.item.email
+			)
+			)
+		)
+	)
+	},
+});
