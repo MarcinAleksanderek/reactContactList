2a416450283b84f2141c3e970738ee6db2298711
Marcin <marcin.aleksanderek@gmail.com>
2019-05-20 23:54:00 +0200

    Update to JSX 2

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,11 +6,11 @@ var ContactForm = React.createClass({
 	render: function() {
 		return (
 			<form className= {'contactForm'}>
-				<input type="text" name="name" placeholder="Imię" className= {'Input-text'}/>
-				<input type="text" name="nazwisko" placeholder="Nazwisko" className= {'Input-text'}/>
-				<input type="email" name="email" placeholder="Email" className= {'Input-text'}/>
+				<input type="text" name="name" placeholder="Imię" className= {'Input-text'} value={this.props.contact.firstName}/>
+				<input type="text" name="nazwisko" placeholder="Nazwisko" className= {'Input-text'} value={this.props.contact.lastname}/>
+				<input type="email" name="email" placeholder="Email" className= {'Input-text'} value={this.props.contact.email}/>
 				<button type="submit" value="Submit" className= {'button'}>Dodaj kontakt</button>
 			</form>
 		)
 	},
-})
+});
